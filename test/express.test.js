93d975759b647e534c290e49db692262a6c2c8ec
Arnob Almazee <arnobxtreme@gmail.com>
2022-05-10 10:19:57 +0600

    express test updated

--- test/express.test.js
+++ test/express.test.js
@@ -2,39 +2,66 @@ const express = require('express');
 const request = require("supertest");
 
 // Create Express Application
-function createApp() {
-    app = express();
+function ExpressInit() {
 
+    let exp = express();
     let router = express.Router();
+
     router.route('/').get(function (req, res) {
-        return res.json({ goodCall: true });
+        return res.json({ status: true });
     });
 
-    app.use(router);
+    router.route('/').post(function (req, res) {
+        return res.json({ status: true });
+    });
 
-    return app;
+    // router
+    exp.use(router);
+
+    return exp;
 }
 
 
-describe('Testing With Express', function () {
-    let app;
+describe('GET - Testing With Express', function () {
+    let exp;
 
     // Called once before any of the tests in this block begin.
     beforeAll(function (done) {
-        app = createApp();
-        app.listen(function (err) {
-            if (err) { return done(err); }
+        exp = ExpressInit();
+        exp.listen(function (err) {
+            if (err) { 
+                console.error("Express init Error");
+                return done(err); 
+            }
             
-            console.log("App Creating Done")
+            console.log("Express init Done");
             done();
         });
     });
     
-    it('should send back a JSON object with goodCall set to true', function (done) {
-        request(app)
+    it('Return request from GET', function (done) {
+        request(exp)
             .get('/')
             .expect(200, function (err, res) {
+                if (err) { 
+                    console.error("Request Error");
+                    return done(err); 
+                }
+
+                console.log("GET - Request Body", res.body);
+                
+                expect(res.statusCode).toBe(200);
+                done();
+            });
+    });
+
+    it('Return request from POST', function (done) {
+        request(exp)
+            .post('/')
+            .expect(200, function (err, res) {
                 if (err) { return done(err); }
+
+                console.log("POST - Request Body", res.body);
                 expect(res.statusCode).toBe(200);
                 done();
             });
